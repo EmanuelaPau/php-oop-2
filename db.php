1260ed1cd0100f05dbdcc91d05e3201afc4fefe0
EmanuelaPau <uapame@gmail.com>
2023-07-14 16:04:27 +0200

    fix db

--- db.php
+++ db.php
@@ -2,29 +2,29 @@
 
 $products = [
     [
-        $name => '',
-        $price => '',
-        $description => '',
-        $image => '',
-        $category => '',
-        $typeOfProduct => '',
+        'name' => 'sphljl',
+        'price' => 'klja',
+        'description' => 'kjsakjsa',
+        'image' => 'sahla',
+        'category' => 'asjka',
+        'typeOfProduct' => 'klksalsa',
     ]
 ];
 
 $categories = [
     [
-        $animal => 'dog',
-        $icon => 'fa-solid fa-dog',
-        $picture => 'https://images.pexels.com/photos/3361739/pexels-photo-3361739.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        'animal' => 'dog',
+        'icon' => 'fa-solid fa-dog',
+        'picture' => 'https://images.pexels.com/photos/3361739/pexels-photo-3361739.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
     ],
     [
-        $animal => 'cat',
-        $icon => 'fa-solid fa-cat',
-        $picture => 'https://images.pexels.com/photos/1317844/pexels-photo-1317844.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        'animal' => 'cat',
+        'icon' => 'fa-solid fa-cat',
+        'picture' => 'https://images.pexels.com/photos/1317844/pexels-photo-1317844.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
     ],
     [
-        $animal => 'bird',
-        $icon => 'fa-solid fa-crow',
-        $picture => 'https://images.pexels.com/photos/13938350/pexels-photo-13938350.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        'animal' => 'bird',
+        'icon' => 'fa-solid fa-crow',
+        'picture' => 'https://images.pexels.com/photos/13938350/pexels-photo-13938350.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
     ],
 ];
