8846e1d2c7c7e3d17a5abe7e468884f9d72129dd
EmanuelaPau <uapame@gmail.com>
2023-07-14 14:55:41 +0200

    Add fontawesome and bootstrap

--- index.php
+++ index.php
@@ -8,3 +8,23 @@ Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, 
 include_once __DIR__ . '/classes/Cats.php';
 include_once __DIR__ . '/classes/Dogs.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Animal Shop</title>
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+
+</body>
+
+</html>
