7906cbd5f240721ecaa61befd833b8e85bdccfd2
EmanuelaPau <uapame@gmail.com>
2023-07-14 15:51:04 +0200

    Add db

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+
+$categories = [
+    [
+        $animal => 'dog',
+        $icon => 'fa-solid fa-dog',
+        $picture => 'https://images.pexels.com/photos/3361739/pexels-photo-3361739.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    ],
+    [
+        $animal => 'cat',
+        $icon => 'fa-solid fa-cat',
+        $picture => 'https://images.pexels.com/photos/1317844/pexels-photo-1317844.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    ],
+    [
+        $animal => 'bird',
+        $icon => 'fa-solid fa-crow',
+        $picture => 'https://images.pexels.com/photos/13938350/pexels-photo-13938350.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    ],
+];
