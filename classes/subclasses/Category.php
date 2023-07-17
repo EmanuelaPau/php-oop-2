94b658f55120a85116b3744c9b6ac18a2c172243
EmanuelaPau <uapame@gmail.com>
2023-07-17 15:02:58 +0200

    Add cat and dogs categories

--- classes/subclasses/Category.php
+++ classes/subclasses/Category.php
@@ -1,18 +1,21 @@
 <?php
 include_once __DIR__ . '/db.php';
 
-class Category extends Product
+class Category
 {
     public $animal;
     public $icon;
     public $picture;
 
-    function __construct($name, $price, $description, $image, $category, $animal, $icon, $picture)
+    function __construct($animal, $icon, $picture)
     {
-        parent::__construct($name, $price, $description, $image, $category);
         $this->animal = $animal;
         $this->icon = $icon;
         $this->picture = $picture;
     }
+}
+
+foreach ($categories as $category) {
+    $category['animal'] = new Category($category['animal'], $category['icon'], $category['picture']);
 }
 ?>
